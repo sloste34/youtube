dfdfb03f8a8deb75ab86894d82cfcef6d39caff3
sloste34 <cocacolazero024@gmail.com>
2023-05-29 23:59:12 +0900

    add react router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
