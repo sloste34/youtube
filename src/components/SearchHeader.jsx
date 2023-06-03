fd95b660f7753f7793286d91350705a66757ef58
sloste34 <cocacolazero024@gmail.com>
2023-06-03 23:57:32 +0900

    create search bar input form

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsYoutube } from 'react-icons/bs';
 
 function SearchHeader() {
+  const [text, setText] = useState('');
   return (
     <header>
       {/* logo part */}
       <div>
         <BsYoutube />
+        <h1>Youtube</h1>
       </div>
       {/* search form part */}
-      <form></form>
+      <form>
+        <input type='text' placeholder='Search...' value={text} />
+      </form>
     </header>
   );
 }
