bbca05099edeef5cb006fcb21f4ad0f16f5fba32
sloste34 <cocacolazero024@gmail.com>
2023-06-14 15:31:41 +0900

    get text from search bar and toss to navitate

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
 import { BsYoutube } from 'react-icons/bs';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 
 function SearchHeader() {
   const [text, setText] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    // 새로고침 방지
+    e.preventDefault();
+
+    console.log(text);
+
+    // 검색한 텍스트를 가지고 /videos/텍스트 로 이동
+    navigate(`/videos/${text}`);
+  };
   return (
     <header>
-      {/* logo part */}
+      {/* 유튜브 로고 들어가는 부분 */}
       <div>
         <BsYoutube />
         <h1>Youtube</h1>
       </div>
-      {/* search form part */}
-      <form>
+      {/* 검색창 들어가는 부분 */}
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           placeholder='Search...'
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
+        <button>
+          <BiSearchAlt2 />
+        </button>
       </form>
     </header>
   );
