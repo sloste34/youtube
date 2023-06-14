b2492d85a21527a18b39261e4e6f2a7110fa6bfe
sloste34 <cocacolazero024@gmail.com>
2023-06-14 20:37:58 +0900

    style on search bar

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -25,21 +25,22 @@ function SearchHeader() {
     navigate(`/videos/${text}`);
   };
   return (
-    <header>
+    <header className='flex p-5 w-full text-3xl border-b border-zinc-600 mb-5 items-center'>
       {/* 유튜브 로고 들어가는 부분 */}
-      <Link to='/'>
-        <BsYoutube />
-        <h1>Youtube</h1>
+      <Link to='/' className='flex items-center'>
+        <BsYoutube className='text-5xl text-brand' />
+        <h1 className='text-5xl font-bold ml-2'>Youtube</h1>
       </Link>
       {/* 검색창 들어가는 부분 */}
-      <form onSubmit={handleSubmit}>
+      <form className='flex w-full justify-center' onSubmit={handleSubmit}>
         <input
+          className='w-7/12 p-2 outline-none bg-black text-gray-50 rounded'
           type='text'
           placeholder='Search...'
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button>
+        <button className='bg-zinc-600 px-4 rounded'>
           <BiSearchAlt2 />
         </button>
       </form>
