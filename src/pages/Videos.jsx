2f5cfad5147759fb6b5b367e8b3970c6417ba320
sloste34 <cocacolazero024@gmail.com>
2023-06-19 23:52:04 +0900

    add react query

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 function Videos() {
   const keyword = useParams().keyword;
+  const { isLoading, error, data: videos } = uesQuery();
   return <h1>Videos of {keyword ? keyword : 'Hot Trend'}</h1>;
 }
 
